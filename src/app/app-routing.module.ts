e08168912c96f05c681aaf03ade29ede0394d177
Daniel Mercado <daniel.mer@hotmail.com>
2021-10-26 10:41:52 -0500

    elimina modulos que no se usaron para la prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
-import { LoginGuard } from './guards/login.guard';
 import { PeliculasComponent } from './pages/peliculas/peliculas.component';
 import { DetailMoviesComponent } from './pages/detail-movies/detail-movies.component';
 
